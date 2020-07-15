95e6a344b91c5c945a62b0adbaca1d363109e370
panida <kiamook1999@gmail.com>
2020-07-15 14:56:13 +0700

    add more code

--- src/Lad1/HelloWorld.java
+++ src/Lad1/HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
-        System.out.println("Hello World !!!");
+        System.out.println("นางสาวพนิดา วังบุญคง");
+        System.out.println("คณะ เทคโนโลยีการจัดการ");
+        System.out.println("สาขา การจัดการเทคโนโลยีการจัดการสารสนเทศ");
+
 
 
     }
