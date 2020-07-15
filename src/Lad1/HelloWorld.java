d8ffa5867398be88e8b3b2b3478591c7bb1dea28
panida <kiamook1999@gmail.com>
2020-07-15 14:31:29 +0700

    Test push

---
+++ src/Lad1/HelloWorld.java
@@ -0,0 +1,15 @@
+package Lad1;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !!!");
+        System.out.println("Hello World !!!");
+
+
+    }
+
+
+
+}
